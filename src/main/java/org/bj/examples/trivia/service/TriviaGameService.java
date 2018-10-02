eb7b57c5670c45d8b7cfe51a52fead38b9ce006f
bhardage <bhardage@gmail.com>
2018-10-02 16:21:22 -0500

    Add simple TriviaGameService

---
+++ src/main/java/org/bj/examples/trivia/service/TriviaGameService.java
@@ -0,0 +1,21 @@
+package org.bj.examples.trivia.service;
+
+import org.bj.examples.trivia.dto.SlackRequestDoc;
+import org.bj.examples.trivia.dto.SlackResponseDoc;
+
+public interface TriviaGameService {
+    SlackResponseDoc start(final SlackRequestDoc requestDoc);
+
+    /**
+     * This method is used when a person is supposed to be
+     * selecting a quote but they don't want to
+     */
+    SlackResponseDoc stop(final SlackRequestDoc requestDoc);
+
+    SlackResponseDoc submitQuestion(final SlackRequestDoc requestDoc, final String question);
+    SlackResponseDoc submitAnswer(final SlackRequestDoc requestDoc, final String answer);
+    SlackResponseDoc markAnswerCorrect(final SlackRequestDoc requestDoc, final String target, final String answer);
+
+    SlackResponseDoc getScores(final SlackRequestDoc requestDoc);
+    SlackResponseDoc resetScores(final SlackRequestDoc requestDoc);
+}
