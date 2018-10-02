03294631e2de69b6222619a9e54c1ed2aa7b0b09
bhardage <bhardage@gmail.com>
2018-10-02 18:06:36 -0500

    Try moving @Bean declarations into TriviaApplication class

--- src/main/java/org/bj/examples/trivia/message/PubSubConfig.java
+++ src/main/java/org/bj/examples/trivia/message/PubSubConfig.java
@@ -1,19 +1,8 @@
 package org.bj.examples.trivia.message;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
-import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-
-@Configuration
+//@Configuration
 public class PubSubConfig {
-    @Bean
+    /*@Bean
     @ServiceActivator(inputChannel = "pubsubOutputChannel")
     public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
         return new PubSubMessageHandler(pubsubTemplate, "delayedMessageTopic");
@@ -33,5 +22,5 @@ public class PubSubConfig {
         adapter.setOutputChannel(inputChannel);
 
         return adapter;
-    }
+    }*/
 }
