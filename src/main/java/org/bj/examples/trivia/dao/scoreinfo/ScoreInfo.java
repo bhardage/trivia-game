41413647b5db88a6fb8b6d14e9eb61c16f4117a9
bhardage <bhardage@gmail.com>
2018-10-09 15:05:46 -0500

    Add ScoreInfo and ScoreInfo DAO classes to interact with cloud datastore

---
+++ src/main/java/org/bj/examples/trivia/dao/scoreinfo/ScoreInfo.java
@@ -0,0 +1,100 @@
+package org.bj.examples.trivia.dao.scoreinfo;
+
+public class ScoreInfo {
+    public static final String ID_KEY = "id";
+    public static final String CHANNEL_ID_KEY = "channelId";
+    public static final String USER_ID_KEY = "userId";
+    public static final String USERNAME_KEY = "username";
+    public static final String SCORE_KEY = "score";
+
+    private Long id;
+    private String channelId;
+    private String userId;
+    private String username;
+    private Long score;
+
+    private ScoreInfo(Builder builder) {
+        this.id = builder.id;
+        this.channelId = builder.channelId;
+        this.userId = builder.userId;
+        this.username = builder.username;
+        this.score = builder.score;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String channelId;
+        private String userId;
+        private String username;
+        private Long score;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder channelId(String channelId) {
+            this.channelId = channelId;
+            return this;
+        }
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder score(Long score) {
+            this.score = score;
+            return this;
+        }
+
+        public ScoreInfo build() {
+            return new ScoreInfo(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+}
---
+++ src/main/java/org/bj/examples/trivia/dao/scoreinfo/ScoreInfoDao.java
@@ -0,0 +1,108 @@
+package org.bj.examples.trivia.dao.scoreinfo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bj.examples.trivia.dao.BaseDao;
+import org.springframework.stereotype.Service;
+
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.IncompleteKey;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.CompositeFilter;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
+
+@Service
+public class ScoreInfoDao extends BaseDao {
+    public ScoreInfoDao() {
+        super("ScoreInfo");
+    }
+
+    public List<ScoreInfo> findAllByChannelId(final String channelId) {
+        final Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(kind)
+                .setFilter(PropertyFilter.eq(ScoreInfo.CHANNEL_ID_KEY, channelId))
+                .setOrderBy(OrderBy.desc(ScoreInfo.SCORE_KEY))
+                .build();
+
+        return asStream(datastore.run(query))
+                .map(this::entityToScoreInfo)
+                .collect(Collectors.toList());
+    }
+
+    public ScoreInfo findByChannelIdAndUserId(final String channelId, final String userId) {
+        final Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(kind)
+                .setFilter(CompositeFilter.and(
+                        PropertyFilter.eq(ScoreInfo.CHANNEL_ID_KEY, channelId),
+                        PropertyFilter.eq(ScoreInfo.USER_ID_KEY, userId)
+                ))
+                .build();
+
+        final QueryResults<Entity> results = datastore.run(query);
+
+        if (results.hasNext()) {
+            return entityToScoreInfo(results.next());
+        } else {
+            return null;
+        }
+    }
+
+    public ScoreInfo save(final ScoreInfo scoreInfo) {
+        if (scoreInfo == null) {
+            return null;
+        }
+
+        Entity scoreInfoEntity = null;
+
+        if (scoreInfo.getId() == null) {
+            final IncompleteKey key = keyFactory.newKey();
+            scoreInfoEntity = datastore.add(scoreInfoToEntity(key, scoreInfo));
+        } else {
+            final Key key = keyFactory.newKey(scoreInfo.getId());
+            datastore.update((Entity)scoreInfoToEntity(key, scoreInfo));
+        }
+
+        return entityToScoreInfo(scoreInfoEntity);
+    }
+
+    public void deleteAllByChannelId(final String channelId) {
+        final Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(kind)
+                .setFilter(PropertyFilter.eq(ScoreInfo.CHANNEL_ID_KEY, channelId))
+                .build();
+
+        final List<Key> keys = asStream(datastore.run(query))
+                .map(entity -> entity.getKey())
+                .collect(Collectors.toList());
+
+        datastore.delete(keys.toArray(new Key[0]));
+    }
+
+    private FullEntity<?> scoreInfoToEntity(final IncompleteKey key, final ScoreInfo scoreInfo) {
+        return Entity.newBuilder(key)
+                .set(ScoreInfo.CHANNEL_ID_KEY, scoreInfo.getChannelId())
+                .set(ScoreInfo.USER_ID_KEY, scoreInfo.getUserId())
+                .set(ScoreInfo.USERNAME_KEY, scoreInfo.getUsername())
+                .set(ScoreInfo.SCORE_KEY, scoreInfo.getScore())
+                .build();
+    }
+
+    private ScoreInfo entityToScoreInfo(final Entity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new ScoreInfo.Builder()
+                .id(entity.getKey().getId())
+                .channelId(entity.getString(ScoreInfo.CHANNEL_ID_KEY))
+                .userId(entity.getString(ScoreInfo.USER_ID_KEY))
+                .username(entity.getString(ScoreInfo.USERNAME_KEY))
+                .score(entity.getLong(ScoreInfo.SCORE_KEY))
+                .build();
+    }
+}
